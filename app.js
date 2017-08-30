b58d688b666bff720e7ba55972116d68e10a0371
saraharless <sara.e.harless@gmail.com>
2017-08-30 11:42:26 -0500

    connected to mustache and added GUI boilerplate

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
-const models = require("./models");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
 
-function createUser() {
-  const user = models.User.build({
-    name: 'Sara Harless',
-    email: 'sara.e.harless@gmail.com',
-    bio: 'designer developer'
-  });
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
-  user.save().then(function (newUser) {
-    console.log(newUser.dataValues);
-  })
-}
-createUser();
 
-function listUsers() {
-  models.User.findAll().then(function(users) {
-    users.forEach(function(user) {
-      console.log(user.dataValues);
-    })
-  })
-}
-listUsers();
+// const models = require("./models");
+
+// function createUser() {
+//   const user = models.User.build({
+//     name: 'Sara Harless',
+//     email: 'sara.e.harless@gmail.com',
+//     bio: 'designer developer'
+//   });
+//
+//   user.save().then(function (newUser) {
+//     console.log(newUser.dataValues);
+//   })
+// }
+// createUser();
+//
+// function listUsers() {
+//   models.User.findAll().then(function(users) {
+//     users.forEach(function(user) {
+//       console.log(user.dataValues);
+//     })
+//   })
+// }
+// listUsers();
+
+app.get('/', function(req,res) {
+  res.render("index");
+})
+
+app.listen(3000, function() {
+  console.log('you did it, or something');
+})
+
+process.on('SIGINT', function() {
+  console.log("\nshutting down");
+  const index = require('./models/index')
+  index.sequelize.close()
+
+ // give it a second
+  setTimeout(function() {
+    console.log('process exit');
+    process.exit(0);
+  }, 1000)
+});
